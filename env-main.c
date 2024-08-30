07aad92d97e1388897f47d6c153087e1686943a2
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-08-29 21:16:41 -0700

    .

---
+++ env-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the environment
+ * @ac: The number of command-line arguments (not used).
+ * @av: The array of command-line arguments (not used).
+ * @env: The array of environment variables.
+ * Return: Always 0.
+ */
+int main(int ac, char **av, char **env)
+{
+	unsigned int i;
+
+	i = 0;
+	while (env[i] != NULL)
+	{
+	printf("%s\n", env[i]);
+	i++;
+	}
+	return (0);
+}
