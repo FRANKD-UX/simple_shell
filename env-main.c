c921647389d3c865befc60d90e5d6f8a53b2de37
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-01 18:03:53 -0700

    .

--- env-main.c
+++ env-main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h" // Make sure to include the header file where necessary
+#include "main.h" /*Make sure to include the header file where necessary*/
 
-// Function prototypes if needed
+/*Function prototypes if needed*/
 void some_function(void);
 
-// Main function
+/*Main function*/
 int main(void)
 {
-    // Your implementation here
+    /*our implementation here*/
     printf("Hello from main!\n");
     
-    // Call other functions if needed
+    /*Call other functions if needed*/
     some_function();
     
     return 0;
 }
 
-// Example additional function
+/*Example additional function*/
 void some_function(void)
 {
     printf("This is an additional function.\n");
