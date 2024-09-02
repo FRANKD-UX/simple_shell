b05d9c1a5a40137e19d0ab7c5b4156e2423cd1ff
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-01 19:38:07 -0700

    .

--- getenv_code3.c
+++ getenv_code3.c
@@ -4,12 +4,15 @@
 
 char *_getenv(const char *name)
 {
-    char **env;
-    for (env = environ; *env != NULL; env++) {
-        if (_strncmp(name, *env, _strlen(name)) == 0) {
-            return (*env + _strlen(name) + 1);
-        }
-    }
-    return (NULL);
+	char **env;
+
+	for (env = environ; *env != NULL; env++)
+	{
+	if (_strncmp(name, *env, _strlen(name)) == 0)
+		{
+		return (*env + _strlen(name) + 1);
+		}
+	}
+	return (NULL);
 }
 
