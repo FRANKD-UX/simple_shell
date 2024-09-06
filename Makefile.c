dd5f018a836dfa8a60174b623ee656047256baa8
FRANKD-UX <frankdafrica@gmail.com>
2024-09-06 13:11:56 -0700

    e

--- Makefile.c
+++ Makefile.c
@@ -1,12 +1,12 @@
 CC = gcc
-CFLAGS = -Wall -Werror -Wextra -pedantic -std=gnu89
+CFLAGS = -Wall - Werror - Wextra - pedantic - std = gnu89
 TARGET = simple_shell
 
-all: $(TARGET)
+all : $(TARGET)
 
-$(TARGET): simple_shell..o
-	$(CC) $(CFLAGS) -o $(TARGET) simple_shell.o
+$(TARGET) : simple_shell.o
+	$(CC) $(CFLAGS) - o $(TARGET) simple_shell.o
 
-clean:
-	rm -f $(TARGET) *.o
+clean :
+	rm - f $(TARGET)  * .o
 
