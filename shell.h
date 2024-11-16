eff4be2a269367a89ab4c9facb745c66ffe15378
FRANKD-UX <frankdafrica@gmail.com>
2024-11-16 01:44:58 -0800

    F

--- shell.h
+++ shell.h
@@ -1,17 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-/* Function prototypes */
 void run_interactive_mode(void);
 void run_non_interactive_mode(const char *script_path);
-void execute_command(const char *command);
-char **tokenize(const char *line);
-int handle_builtin(char **args);
-void free_tokens(char **tokens);
+void execute_command(char *command);
 
 #endif /* SHELL_H */
+
