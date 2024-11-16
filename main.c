315b7ff4c01c4a84744e8706de20cb37bb927227
FRANKD-UX <frankdafrica@gmail.com>
2024-11-16 01:54:48 -0800

    F

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
 #include "shell.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point for the shell
  * @argc: Argument count
  * @argv: Argument vector
  *
- * Return: 0 on success
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char **argv)
 {
-	if (argc == 2)
+	if (argc > 1)
 	{
+		/* Non-interactive mode */
 		run_non_interactive_mode(argv[1]);
-	}
-	else if (argc == 1)
-	{
-		run_interactive_mode();
 	}
 	else
 	{
-		fprintf(stderr, "Usage: %s [script_path]\n", argv[0]);
-		return (1);
+		/* Interactive mode */
+		run_interactive_mode();
 	}
-
 	return (0);
 }
 
--- process_command.c
+++ process_command.c
@@ -1,37 +1,51 @@
 #include "shell.h"
-#include <stdlib.h>
+#include <string.h>
 
-/**
- * execute_command - Executes a shell command
- * @command: Command to execute
- */
 void execute_command(const char *command)
 {
-	char **args;
-	pid_t pid;
+    pid_t pid;
+    int status;
+    char **args;
 
-	args = tokenize(command);
-	if (args == NULL)
-		return;
+    /* Tokenize command */
+    args = tokenize(command);
+    if (args == NULL)
+    {
+        perror("Failed to tokenize command");
+        return;
+    }
 
-	if (!handle_builtin(args))
-	{
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execvp(args[0], args) == -1)
-				perror("Error executing command");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid < 0)
-		{
-			perror("Error forking");
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
+    /* Handle built-in commands */
+    if (handle_builtin(args))
+    {
+        free_tokens(args);
+        return;
+    }
 
-	free_tokens(args);
+    /* Fork process */
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("Error during fork");
+        free_tokens(args);
+        return;
+    }
+
+    if (pid == 0) /* Child process */
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("Error executing command");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else /* Parent process */
+    {
+        do {
+            wait(&status);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    free_tokens(args);
 }
+
