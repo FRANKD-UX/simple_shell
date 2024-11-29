b07205f027e9626204f50aa7571de85eb01d5c93
FRANKD-UX <frankdafrica@gmail.com>
2024-11-29 11:14:32 -0800

    task1

--- simple_shell.c
+++ simple_shell.c
@@ -1,108 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
 
-void print_prompt(void);
-char *read_input(void);
-char **parse_input(char *input);
+/* Function Prototypes */
 void execute_command(char **args);
+char *resolve_path(char *command);
+void free_args(char **args);
+char **parse_input(char *input);
 
 /**
- * main - Entry point for the simple shell.
+ * main - Entry point of the shell program.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *input;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 	char **args;
 
 	while (1)
 	{
-		print_prompt();
-		input = read_input();
-		if (input == NULL)
-			break; /* Exit on Ctrl+D */
+		/* Display prompt */
+		printf("#cisfun$ ");
+		fflush(stdout);
 
-		args = parse_input(input);
-		if (args[0] == NULL)
+		/* Read user input */
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
 		{
-			free(input);
-			free(args);
-			continue; /* Skip empty input */
+			free(line);
+			break;
 		}
 
-		execute_command(args);
+		/* Remove newline character */
+		line[strcspn(line, "\n")] = '\0';
 
-		free(input);
-		free(args);
+		/* Parse input into arguments */
+		args = parse_input(line);
+		if (!args)
+			continue;
+
+		/* Exit shell */
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free_args(args);
+			break;
+		}
+
+		/* Execute command */
+		execute_command(args);
+		free_args(args);
 	}
 
 	return (0);
 }
 
 /**
- * print_prompt - Prints the shell prompt.
- */
-void print_prompt(void)
-{
-	write(STDOUT_FILENO, "#cisfun$ ", 9);
-}
-
-/**
- * read_input - Reads user input from stdin.
- *
- * Return: Pointer to the input string, or NULL on EOF.
- */
-char *read_input(void)
-{
-	char *buffer = malloc(BUFFER_SIZE);
-
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(EXIT_FAILURE);
-	}
-
-	if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
-	{
-		free(buffer);
-		return (NULL);
-	}
-
-	buffer[strcspn(buffer, "\n")] = '\0'; /* Remove newline character */
-	return (buffer);
-}
-
-/**
- * parse_input - Tokenizes user input into an array of arguments.
+ * parse_input - Parses the input string into arguments.
  * @input: The input string.
  *
- * Return: Pointer to the array of arguments.
+ * Return: Array of arguments or NULL if input is empty.
  */
 char **parse_input(char *input)
 {
-	char **args = malloc(10 * sizeof(char *));
-	int i = 0;
+	char **args = NULL;
+	char *token;
+	size_t idx = 0, bufsize = BUFFER_SIZE;
 
-	if (args == NULL)
+	if (!input || strlen(input) == 0)
+		return (NULL);
+
+	args = malloc(sizeof(char *) * bufsize);
+	if (!args)
 	{
-		perror("Unable to allocate args");
-		exit(EXIT_FAILURE);
+		perror("malloc");
+		return (NULL);
 	}
 
-	args[i] = strtok(input, " ");
-	while (args[i] != NULL && i < 9)
+	token = strtok(input, " ");
+	while (token)
 	{
-		i++;
-		args[i] = strtok(NULL, " ");
+		args[idx++] = strdup(token);
+		if (idx >= bufsize)
+		{
+			bufsize += BUFFER_SIZE;
+			args = realloc(args, sizeof(char *) * bufsize);
+			if (!args)
+			{
+				perror("realloc");
+				return (NULL);
+			}
+		}
+		token = strtok(NULL, " ");
 	}
-	args[9] = NULL;
+	args[idx] = NULL;
 
 	return (args);
 }
@@ -115,12 +114,26 @@ void execute_command(char **args)
 {
 	pid_t pid;
 	int status;
+	char *command;
+	struct stat st;
+
+	/* Resolve command using PATH if not an absolute/relative path */
+	command = args[0];
+	if (stat(command, &st) != 0)
+	{
+		command = resolve_path(args[0]);
+		if (!command)
+		{
+			fprintf(stderr, "%s: Command not found\n", args[0]);
+			return;
+		}
+	}
 
 	pid = fork();
 	if (pid == 0)
 	{
 		/* Child process */
-		if (execve(args[0], args, NULL) == -1)
+		if (execve(command, args, NULL) == -1)
 		{
 			perror(args[0]);
 			exit(EXIT_FAILURE);
@@ -135,5 +148,67 @@ void execute_command(char **args)
 	{
 		perror("Fork failed");
 	}
+
+	/* Free the resolved command if allocated */
+	if (command != args[0])
+		free(command);
+}
+
+/**
+ * resolve_path - Searches for a command in the PATH environment variable.
+ * @command: The command to search for.
+ *
+ * Return: Full path to the command, or NULL if not found.
+ */
+char *resolve_path(char *command)
+{
+	char *path = getenv("PATH");
+	char *path_dup = strdup(path);
+	char *dir, *full_path = NULL;
+	struct stat st;
+
+	if (!path_dup)
+		return (NULL);
+
+	dir = strtok(path_dup, ":");
+	while (dir)
+	{
+		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		if (!full_path)
+		{
+			free(path_dup);
+			return (NULL);
+		}
+
+		sprintf(full_path, "%s/%s", dir, command);
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_dup);
+			return (full_path); /* Found the command */
+		}
+
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_dup);
+	return (NULL); /* Not found */
+}
+
+/**
+ * free_args - Frees an array of arguments.
+ * @args: The array of arguments.
+ */
+void free_args(char **args)
+{
+	size_t i = 0;
+
+	if (!args)
+		return;
+
+	while (args[i])
+		free(args[i++]);
+
+	free(args);
 }
 
