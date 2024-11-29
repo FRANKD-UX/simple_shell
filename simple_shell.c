bf117c3be94a39c904c6e167a0de732c10868e88
FRANKD-UX <frankdafrica@gmail.com>
2024-11-29 10:03:43 -0800

    taks1

--- simple_shell.c
+++ simple_shell.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #define PROMPT "#cisfun$ "
 
@@ -30,6 +33,63 @@ void handle_getline_error(ssize_t read)
 }
 
 /**
+ * execute_command - Forks a child process to execute a command.
+ * @command: The command to execute.
+ *
+ * Return: Void.
+ */
+void execute_command(char *command)
+{
+	pid_t pid;
+	char *argv[] = {command, NULL};
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(command, argv, environ) == -1)
+		{
+			fprintf(stderr, "%s: No such file or directory\n", command);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+}
+
+/**
+ * list_directory_contents - Lists the contents of the current directory.
+ *
+ * Return: Void.
+ */
+void list_directory_contents(void)
+{
+	DIR *dir;
+	struct dirent *entry;
+
+	dir = opendir(".");
+	if (dir == NULL)
+	{
+		perror("opendir");
+		return;
+	}
+
+	while ((entry = readdir(dir)) != NULL)
+	{
+		printf("%s\n", entry->d_name);
+	}
+
+	closedir(dir);
+}
+
+/**
  * main - Entry point for the simple shell program.
  *
  * Return: Always 0.
@@ -40,38 +100,23 @@ int main(void)
 	size_t len = 0;
 	ssize_t read;
 	char *command;
-	char *argv[] = {NULL, NULL};
-	int status;
-	pid_t pid;
 
 	while (1)
 	{
 		printf(PROMPT);
 		read = getline(&line, &len, stdin);
-		handle_getline_error(read);  /* Call the error handling function */
+		handle_getline_error(read);
 
 		line[strcspn(line, "\n")] = '\0';
 		command = line;
-		argv[0] = command;
 
-		pid = fork();
-
-		if (pid == -1)
+		if (strcmp(command, "ls") == 0)
 		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (execve(command, argv, environ) == -1)
-			{
-				fprintf(stderr, "%s: No such file or directory\n", command);
-				exit(EXIT_FAILURE);
-			}
+			list_directory_contents();
 		}
 		else
 		{
-			wait(&status);
+			execute_command(command);
 		}
 	}
 
